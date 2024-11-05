c4453843f11e5c0b6320bd0827b68479b382e801
thangshelby <n.nducthangg@gmail.com>
2024-11-05 09:15:58 +0700

    change UI onboarding

--- client/app/(auth)/welcome.tsx
+++ client/app/(auth)/welcome.tsx
@@ -1,9 +1,17 @@
 import { router } from "expo-router";
 import { useRef, useState } from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Animated,
+  Easing,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
-
+import { LinearGradient } from "expo-linear-gradient";
 import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/constants";
 
@@ -14,57 +22,119 @@ const Home = () => {
   const isLastSlide = activeIndex === onboarding.length - 1;
 
   return (
-    <SafeAreaView className="flex h-full items-center justify-between bg-white">
-      <TouchableOpacity
-        onPress={() => {
-          router.replace("/(auth)/sign-up");
-        }}
-        className="w-full flex justify-end items-end p-5"
-      >
-        <Text className="text-black text-md font-JakartaBold">Skip</Text>
-      </TouchableOpacity>
-
+    <SafeAreaView className="flex-col h-full  items-center pb-16 justify-between bg-[#181858]">
       <Swiper
         ref={swiperRef}
         loop={false}
-        dot={
-          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
-        }
-        activeDot={
-          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
-        }
+        dot={<View className="p-1 mx-1 bg-[#E2E8F0] rounded-full" />}
+        activeDot={<View className="p-1 mx-1 bg-yellow-400 rounded-full" />}
         onIndexChanged={(index) => setActiveIndex(index)}
       >
-        {onboarding.map((item) => (
-          <View key={item.id} className="flex items-center justify-center p-5">
-            <Image
-              source={item.image}
-              className="w-full h-[300px]"
-              resizeMode="contain"
-            />
-            <View className="flex flex-row items-center justify-center w-full mt-10">
-              <Text className="text-black text-3xl font-bold mx-10 text-center">
-                {item.title}
-              </Text>
-            </View>
-            <Text className="text-md font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
-              {item.description}
+        <View className="flex-col justify-center items-center">
+          <Image
+            className="w-full mt-16"
+            resizeMode="stretch"
+            source={require("@/assets/images/onboard1.png")}
+          />
+          <View className="mt-10"> 
+            <Text className="text-[30px] text-white font-JakartaSemiBold text-center">
+              SO SÁNH GIÁ
+            </Text>
+            <Text className="text-[20px] text-yellow-500 font-JakartaSemiBold text-center">
+              Dễ dàng giữa các bãi đỗ
             </Text>
           </View>
-        ))}
+        </View>
+        <View className="flex-col justify-center items-center">
+          <Image
+            className="w-full mt-12"
+            resizeMode="stretch"
+            source={require("@/assets/images/onboard2.png")}
+          />
+          <View className="mt-10"> 
+            <Text className="text-[30px] text-white font-JakartaSemiBold text-center">
+              ĐẶT LỊCH
+            </Text>
+            <Text className="text-[20px] text-yellow-500 font-JakartaSemiBold text-center">
+              Nhanh chóng và tiện lợi
+            </Text>
+          </View>
+        </View>
+        <View className="flex-col justify-center items-center">
+          <Image
+            className="w-full mt-16"
+            resizeMode="stretch"
+            source={require("@/assets/images/onboard3.png")}
+          />
+          <View className=""> 
+            <Text className="text-[30px] text-white font-JakartaSemiBold text-center">
+              TÌM KIẾM BÃI ĐỖ
+            </Text>
+            <Text className="text-[20px] text-yellow-500 font-JakartaSemiBold text-center">
+              Ở khắp mọi nơi
+            </Text>
+          </View>
+        </View>
       </Swiper>
 
-      <CustomButton
-        title={isLastSlide ? "Get Started" : "Next"}
-        onPress={() =>
+      <TouchableOpacity
+        onPress={() => {
           isLastSlide
-            ? router.replace("/(auth)/sign-up")
-            : swiperRef.current?.scrollBy(1)
-        }
-        className="w-11/12 mt-10 mb-5"
-      />
+          ? router.replace("/(auth)/sign-up")
+          : swiperRef.current?.scrollBy(1)
+        }}
+        className="w-full p-3 bg-yellow-400 rounded-md"
+      >
+        <Text className="font-JakartaBold text-[#151551] text-center">
+          Tiếp theo
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
 export default Home;
+const styles = StyleSheet.create({
+  view: {
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 10,
+    transform: [{ rotate: "25deg" }],
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#002a5c",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    width: 250,
+    height: 150,
+    borderRadius: 10,
+    marginBottom: 20,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 10,
+  },
+  gradient: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    opacity: 0.9,
+  },
+  cardText: {
+    fontSize: 18,
+    color: "#ffcc00",
+    fontWeight: "bold",
+  },
+  cardSubText: {
+    fontSize: 14,
+    color: "#fff",
+    marginTop: 5,
+  },
+});
